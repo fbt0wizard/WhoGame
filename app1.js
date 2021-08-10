d36debc78d77a9919614c18b18168a80981aade0
fbt0wizard <oladipomustapha001@gmail.com>
2021-08-10 12:21:23 -0400

    games

--- app1.js
+++ app1.js
@@ -3,32 +3,49 @@
 
 
 function getInput() {
-    window.userInput = document.getElementById('userInput').value;
-    console.log(userInput);
-    alertUserInput();
-    document.getElementById("user-name").innerHTML = ("Ranger" + " " + userInput);
-    var home = document.getElementById("firstSection");
-    if (home.style.display === "none") {
-        home.style.display = "block";
-      } else {
-        home.style.display = "none";
-      }
+  window.userInput = document.getElementById('userInput').value;
+  console.log(userInput);
+  alertUserInput();
+  document.getElementById("user-name").innerHTML = ("Let's Play," + " " + userInput);
+  var home = document.getElementById("firstSection");
+  if (home.style.display === "none") {
+    home.style.display = "block";
+  } else {
+    home.style.display = "none";
+  }
 
-    var game = document.getElementById("gameSection");
-    if (game.style.display === "block") {
-        game.style.display = "none";
-      } else {
-        game.style.display = "block";
-      }
+  var game = document.getElementById("gameSection");
+  if (game.style.display === "block") {
+    game.style.display = "none";
+  } else {
+    game.style.display = "block";
+  }
+
+  var score = document.getElementById("marks");
+  score.style.visibility = "hidden";
 }
 
 function alertUserInput() {
-    alert("Welcome" + " " + userInput);
+  alert("Welcome" + " " + userInput);
 }
 
-var subButton = document.getElementById('subButton');
-subButton.addEventListener('click', userInput);
+// var subButton = document.getElementById('subButton');
+// subButton.addEventListener('click', userInput);
 
+function getInput2() {
+  window.userInput2 = document.getElementById('answerText').value;
+  const buhari = "black nigerian politician black alive black african above50 male";
+  var data = userInput2;
+  console.log(buhari.includes(data));
+  if(buhari.includes(data) == true) {
+    let score = document.getElementById("correct");
+    score.style.visibility = "visible";
+  } else {
+    let score2 = document.getElementById("wrong")
+    score2.style.visibility = "visible";
+  }
+    
+}
 
 
 
