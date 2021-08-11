c9de6a09a5cdb9f2d1ba6c208293e10a9d644540
fbt0wizard <oladipomustapha001@gmail.com>
2021-08-11 19:51:00 -0400

    update games

--- app1.js
+++ app1.js
@@ -1,63 +1,101 @@
 // const Buhari = ("black", "Nigerian", "politician", "alive", "african", "above50", "male"); 
 // const Biden = ("american", "white", "politician", "alive", "above50", "male", "west");
 
-
 function getInput() {
   window.userInput = document.getElementById('userInput').value;
-  console.log(userInput);
-  alertUserInput();
-  document.getElementById("user-name").innerHTML = ("Let's Play," + " " + userInput);
+  localStorage.setItem("userEntry", userInput);
+
+  let newData = localStorage.getItem('userEntry');
+  alert("Welcome" + " " + newData);
+  document.getElementById("user-name").innerHTML = ("Let's Play," + " " + newData);
+
   var home = document.getElementById("firstSection");
-  if (home.style.display === "none") {
-    home.style.display = "block";
-  } else {
-    home.style.display = "none";
-  }
+   if (home.style.display === "block") {
+   home.style.display = "none";
+   } else {
+     home.style.display = "none";
+   }
 
-  var game = document.getElementById("gameSection");
-  if (game.style.display === "block") {
-    game.style.display = "none";
-  } else {
-    game.style.display = "block";
-  }
+    var game = document.getElementById("gameSection");
+    if (game.style.display === "none") {
+      game.style.display = "block";
+    } else {
+      game.style.display = "block";
+    }
 
-  var score = document.getElementById("marks");
-  score.style.visibility = "hidden";
+    var score = document.getElementById("marks");
+    score.style.visibility = "hidden";
+  
 }
 
-function alertUserInput() {
-  alert("Welcome" + " " + userInput);
+function clearData() {
+  localStorage.clear();
 }
 
-// var subButton = document.getElementById('subButton');
-// subButton.addEventListener('click', userInput);
+var playerInput = JSON.parse(localStorage.getItem("gameInput") || "[]");
+
 
 function getInput2() {
-  window.userInput2 = document.getElementById('answerText').value;
-  console.log(userInput2.length);
-  if(userInput2 == 0) {
-    alert("Please Input Answer");
+  const buhari = "black nigerian politician alive african above50 male president west africa father married ";
+  var data = document.getElementById('answerText').value;
+  if (data.length == 0) {
+    alert("Please input answer");
     return;
   }
-  const buhari = "black nigerian politician alive african above50 male president west africa";
-  var data = userInput2;
-  console.log(buhari.includes(data));
-  if(buhari.includes(data) == true) {
+
+  var storedResult = localStorage.getItem("gameInput");
+
+  console.log(storedResult);
+  
+ 
+  
+  if(storedResult == null) {
+    if (buhari.includes(data)) {
+      let score = document.getElementById("correct");
+      score.style.visibility = "visible";
+      setTimeout(() => {
+      score.style.visibility = "hidden";
+    }, 3000)
+      
+    } else {
+      let score2 = document.getElementById("wrong")
+      score2.style.visibility = "visible";
+      setTimeout(() => {
+        score2.style.visibility = "hidden";
+      }, 3000);
+      return;
+    
+    }
+    document.getElementById("listUp").innerHTML = data;    
+  }
+  if(storedResult != null) {
+    if(storedResult.includes(data)) {
+      alert("You can't input" + " " + data + " " + "again");
+      return;
+    }
+  }
+  if (buhari.includes(data)) {
     let score = document.getElementById("correct");
     score.style.visibility = "visible";
     setTimeout(() => {
-      score.style.visibility = "hidden";
-    }, 3000)
-    
-  
+    score.style.visibility = "hidden";
+  }, 3000)
+  playerInput.push(data);
+  localStorage.setItem("gameInput", JSON.stringify(playerInput));
   } else {
     let score2 = document.getElementById("wrong")
     score2.style.visibility = "visible";
     setTimeout(() => {
       score2.style.visibility = "hidden";
-    }, 3000)
-  }
-    
+    }, 3000);
+    return;
+  
+  }    
+
+
+  let guide = localStorage.getItem("gameInput")
+  document.getElementById("listUp").innerHTML = guide;
+
 }
 
 
